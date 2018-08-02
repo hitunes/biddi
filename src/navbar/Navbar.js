f2ceb9a1d6acfbfadf3420364b59fb66ef8a5dcd
Itunu Adekoya <hitunes247@gmail.com>
2018-08-02 22:22:23 +0100

    navlist

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
     <div className="nav-wrapper">
       <div className="nav-title">BidiiBuild</div>
       <div className="nav-right">
-        <span>A</span>
-        <span>B</span>
+        <span>
+          <img src="/images/menu.png" alt="menu-btn" />
+        </span>
+        <span>
+          <img src="/images/side-menu.png" alt="menu-btn" />
+        </span>
       </div>
+    </div>
+  );
+};
+
+export const NavList = () => {
+  return (
+    <div className="navlist-wrapper">
+      <ul>
+        <li>Dashboard</li>
+        <li>Inventory</li>
+        <li>Purchase orders</li>
+        <li>bills</li>
+        <li>reciepts</li>
+        <li>stock control</li>
+        <li>reports</li>
+      </ul>
     </div>
   );
 };
