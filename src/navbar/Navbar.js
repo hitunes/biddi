af7f9d3d64d6045215479f37e17593c90b262168
Itunu Adekoya <hitunes247@gmail.com>
2018-08-02 21:42:28 +0100

    navbar

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav-wrapper">
+      <div className="nav-title">BidiiBuild</div>
+      <div className="nav-right">
+        <span>A</span>
+        <span>B</span>
+      </div>
+    </div>
+  );
+};
